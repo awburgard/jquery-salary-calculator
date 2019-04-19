64dd5f7468615d535f6c1edacae41affe4771df9
Austin Burgard <austinburgard@Austins-MacBook-Pro.local>
2019-04-19 15:26:08 -0500

    adding array

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 $(document).ready(onReady);
 
+let employeeArray = [];
 
+peopleArray.push({
+    firstName: 'Jen',
+    lastName: 'Barber',
+    id: 4521,
+    title: 'Team Lead',
+    annualSalary: 800000,
+})
 
 
 function onReady(){
