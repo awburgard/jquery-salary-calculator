4d3ce9bf94edb4acf536bbfbcf0a157d3a9508f6
Austin Burgard <austinburgard@Austins-MacBook-Pro.local>
2019-04-19 15:36:04 -0500

    working on adding monthly costs

--- app.js
+++ app.js
@@ -2,15 +2,35 @@ $(document).ready(onReady);
 
 let employeeArray = [];
 
-peopleArray.push({
+employeeArray.push({
     firstName: 'Jen',
     lastName: 'Barber',
     id: 4521,
     title: 'Team Lead',
     annualSalary: 800000,
-})
+});
 
+employeeArray.push({
+    firstName: 'Maurice',
+    lastName: 'Moss',
+    id: 8724,
+    title: 'Support Team',
+    annualSalary: 58000,
+});
+
+employeeArray.push({
+    firstName: 'Roy',
+    lastName: 'Smith',
+    id: 9623,
+    title: 'Quality Assurance',
+    annualSalary: 48000,
+});
+
+function monthlyCosts(employee){
+    for (let i = 0; i < employee.length; i++){
+        console.log('Hey, I work.');
+    }
+}
 
 function onReady(){
     console.log('Hello');
-}
