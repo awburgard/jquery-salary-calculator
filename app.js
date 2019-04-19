10d9cb53f6b4c9ea6875e422971f4116e26d8c5f
Austin Burgard <austinburgard@Austins-MacBook-Pro.local>
2019-04-19 16:19:06 -0500

    added empty to fix delete button

--- app.js
+++ app.js
@@ -36,23 +36,33 @@ function clickSubmitButton(){
     monthlyCosts();
 }
 function monthlyCosts(){
+    $('.container').empty();
     for (let i = 0; i < employeeArray.length; i++){
         $('.container').append('<div></div>');
         let newDiv = $('.container').children().last();
         let newEmployee = employeeArray[i];
         newDiv.data ('id',i);
-        newDiv.append('<p>' + newEmployee.firstName+ '</p>');
-        newDiv.append('<p>' + newEmployee.lastName+ '</p>');
-        newDiv.append('<p>' + newEmployee.id+ '</p>');
-        newDiv.append('<p>' + newEmployee.title+ '</p>');
-        newDiv.append('<p>' + newEmployee.annualSalary+ '</p>');
-        newDiv.append('<button class = "deleteBtn">Delete</button>');
+        newDiv.append('<p>' + newEmployee.firstName + '</p>');
+        newDiv.append('<p>' + newEmployee.lastName + '</p>');
+        newDiv.append('<p>' + newEmployee.id + '</p>');
+        newDiv.append('<p>' + newEmployee.title + '</p>');
+        newDiv.append('<p>' + newEmployee.annualSalary + '</p>');
+        newDiv.append('<button class="deleteBtn">Delete</button>');
     }
+}
+
+function clickDeleteButton(){
+    let employeeToDelete = $(this).parent().data('id');
+    employeeArray.splice(employeeToDelete, 1);
+    monthlyCosts();
 }
 
 function onReady(){
     monthlyCosts();
-    $('#newEmployee').on('click', clickSubmitButton);
-    
+
+    $('.container').on('click', '.deleteBtn', clickDeleteButton);
+    $('#newEmployee').on('submit', clickSubmitButton);
+
+
     console.log('Hello');
 }
